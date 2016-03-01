9c38910c9c6d66417c7e44762857b6781b0d38fe
Cephei <cepheidev+git@gmail.com>
2016-03-02 00:57:25 +0100

    Updated the weighting function

--- DankScore.lua
+++ DankScore.lua
@@ -26,6 +26,7 @@ end
 
 -- weighting function
 -- using http://wowwiki.wikia.com/wiki/Item_level#Stat_modifiers for stat weighting
+-- scratch that, we're eyeballing the entire thing
 local function weight(stat, value)
   local modified = 0;
 
@@ -34,53 +35,53 @@ local function weight(stat, value)
   elseif stat == "STA"               then modified = value;        -- stamina
   elseif stat == "INT"               then modified = value;        -- intellect
   elseif stat == "SPI"               then modified = value;        -- spirit
-  elseif stat == "ARMOR"             then                          -- reinforced armor (not base armor)
+  elseif stat == "ARMOR"             then modifier = value / 50;   -- reinforced armor (not base armor)
 
-  elseif stat == "ARCANERES"         then modified = value;        -- arcane resistance
-  elseif stat == "FIRERES"           then modified = value;        -- fire resistance
-  elseif stat == "NATURERES"         then modified = value;        -- nature resistance
-  elseif stat == "FROSTRES"          then modified = value;        -- frost resistance
-  elseif stat == "SHADOWRES"         then modified = value;        -- shadow resistance
+  elseif stat == "ARCANERES"         then                          -- arcane resistance
+  elseif stat == "FIRERES"           then                          -- fire resistance
+  elseif stat == "NATURERES"         then                          -- nature resistance
+  elseif stat == "FROSTRES"          then                          -- frost resistance
+  elseif stat == "SHADOWRES"         then                          -- shadow resistance
 
   elseif stat == "FISHING"           then                          -- fishing skill
   elseif stat == "MINING"            then                          -- mining skill
   elseif stat == "HERBALISM"         then                          -- herbalism skill
   elseif stat == "SKINNING"          then                          -- skinning skill
-  elseif stat == "DEFENSE"           then modified = value * 0.83; -- defense skill
+  elseif stat == "DEFENSE"           then modified = value * 4;    -- defense skill
 
-  elseif stat == "BLOCK"             then modified = value * 1.67; -- chance to block
-  elseif stat == "BLOCKVALUE"        then modified = value * 1.67; -- increased block value
-  elseif stat == "DODGE"             then                          -- chance to dodge
-  elseif stat == "PARRY"             then                          -- chance to parry
-  elseif stat == "ATTACKPOWER"       then                          -- attack power
-  elseif stat == "ATTACKPOWERUNDEAD" then modified = value * 3;    -- attack power against undead
-  elseif stat == "ATTACKPOWERFERAL"  then                          -- attack power in feral form
+  elseif stat == "BLOCK"             then modified = value * 14;   -- chance to block
+  elseif stat == "BLOCKVALUE"        then modified = value * 0.5;  -- increased block value
+  elseif stat == "DODGE"             then modified = value * 18;   -- chance to dodge
+  elseif stat == "PARRY"             then modified = value * 20;   -- chance to parry
+  elseif stat == "ATTACKPOWER"       then modified = value * 0.75  -- attack power
+  elseif stat == "ATTACKPOWERUNDEAD" then modified = value * 0.5;  -- attack power against undead
+  elseif stat == "ATTACKPOWERFERAL"  then modified = value * 0.35; -- attack power in feral form
 
-  elseif stat == "CRIT"              then modified = value * 14;   -- chance to get a critical strike
-  elseif stat == "RANGEDATTACKPOWER" then modified = value;        -- ranged attack power
-  elseif stat == "RANGEDCRIT"        then modified = value;        -- chance to get a crit with ranged weapons
-  elseif stat == "TOHIT"             then modified = value;        -- chance to hit
+  elseif stat == "CRIT"              then modified = value * 13;   -- chance to get a critical strike
+  elseif stat == "RANGEDATTACKPOWER" then modified = value * 0.65; -- ranged attack power
+  elseif stat == "RANGEDCRIT"        then modified = value * 11;   -- chance to get a crit with ranged weapons
+  elseif stat == "TOHIT"             then modified = value * 13;   -- chance to hit
 
-  elseif stat == "DMG"               then modified = value;        -- spell damage
-  elseif stat == "DMGUNDEAD"         then modified = value;        -- spell damage against undead
+  elseif stat == "DMG"               then modified = value * 0.66; -- spell damage
+  elseif stat == "DMGUNDEAD"         then modified = value * 0.75; -- spell damage against undead
 
-  elseif stat == "ARCANEDMG"         then modified = value;        -- arcane spell damage
-  elseif stat == "FIREDMG"           then modified = value;        -- fire spell damage
-  elseif stat == "FROSTDMG"          then modified = value;        -- frost spell damage
-  elseif stat == "HOLYDMG"           then modified = value;        -- holy spell damage
-  elseif stat == "NATUREDMG"         then modified = value;        -- nature spell damage
-  elseif stat == "SHADOWDMG"         then modified = value;        -- shadow spell damage
-  elseif stat == "SPELLCRIT"         then modified = value;        -- chance to crit with spells
-  elseif stat == "HEAL"              then modified = value;        -- healing
-  elseif stat == "HOLYCRIT"          then modified = value;        -- chance to crit with holy spells
-  elseif stat == "SPELLTOHIT"        then modified = value;        -- Chance to Hit with spells
+  elseif stat == "ARCANEDMG"         then modified = value * 0.75; -- arcane spell damage
+  elseif stat == "FIREDMG"           then modified = value * 0.75; -- fire spell damage
+  elseif stat == "FROSTDMG"          then modified = value * 0.75; -- frost spell damage
+  elseif stat == "HOLYDMG"           then modified = value * 0.75; -- holy spell damage
+  elseif stat == "NATUREDMG"         then modified = value * 0.75; -- nature spell damage
+  elseif stat == "SHADOWDMG"         then modified = value * 0.75; -- shadow spell damage
+  elseif stat == "SPELLCRIT"         then modified = value * 13;   -- chance to crit with spells
+  elseif stat == "HEAL"              then modified = value * 0.33; -- healing
+  elseif stat == "HOLYCRIT"          then modified = value * 13;   -- chance to crit with holy spells
+  elseif stat == "SPELLTOHIT"        then modified = value * 13;   -- Chance to Hit with spells
 
-  elseif stat == "SPELLPEN"          then modified = value;        -- amount of spell resist reduction
+  elseif stat == "SPELLPEN"          then                          -- amount of spell resist reduction
 
-  elseif stat == "HEALTHREG"         then modified = value;        -- health regeneration per 5 sec.
-  elseif stat == "MANAREG"           then modified = value;        -- mana regeneration per 5 sec.
-  elseif stat == "HEALTH"            then                          -- health points
-  elseif stat == "MANA"              then                          -- mana points
+  elseif stat == "HEALTHREG"         then modified = value / 5;    -- health regeneration per 5 sec.
+  elseif stat == "MANAREG"           then modified = value * 7;    -- mana regeneration per 5 sec.
+  elseif stat == "HEALTH"            then modified = value / 10;   -- health points
+  elseif stat == "MANA"              then modified = value / 10;   -- mana points
   end
 
   return modified;
